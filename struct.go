19b98dc359b86d2059e676d518ee30337c287178
Chyroc <chyroc@qq.com>
2018-08-09 22:24:56 +0800

    fix camel

--- struct.go
+++ struct.go
@@ -3,8 +3,9 @@ package main
 import (
 	"sort"
 	"fmt"
-	"strings"
 	"bytes"
+
+	"github.com/serenize/snaker"
 )
 
 func mapToStruct(buf *bytes.Buffer, padding string, m map[string]interface{}) {
@@ -20,12 +21,13 @@ func mapToStruct(buf *bytes.Buffer, padding string, m map[string]interface{}) {
 		keys = append(keys, k)
 	}
 
-	sort.StringsAreSorted(keys)
+	sort.Strings(keys)
 
 	for _, k := range keys {
 		n := new(bytes.Buffer)
 		interfaceToType(n, newPadding, m[k])
-		buf.WriteString(fmt.Sprintf(newPadding+"%s\t%s\n", strings.Title(k), n.String()))
+
+		buf.WriteString(fmt.Sprintf(newPadding+"%s\t%s\n", snaker.SnakeToCamel(k), n.String()))
 	}
 	buf.WriteString(padding + "}")
 }
